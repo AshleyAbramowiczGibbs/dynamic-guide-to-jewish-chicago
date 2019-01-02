b768a25ccb4ea1f20ceb453e8a111b2b8a9d0091
AshleyAbramowiczGibbs <ashleyabramo2@gmail.com>
2019-01-01 23:56:27 -0600

    experimenting

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,21 @@
 <template>
 <div class="home">
-    <h1>All organizations</h1>
+    <h1>Jewish Chicago Guide</h1>
+    <h2>Did you know? Chicago is the 5th largest Jewish community in the United States of America. Read all about the amazing Jewish organizations operating in the city of Chicago. Click on the categories to find your community. Find Your Tribe. 
+
+</h2>
     <div v-for="organization in organizations">
+  <div>
     <h2>{{ organization.name }}</h2>
     <h3>{{ organization.content }}</h3>
+    <h3>{{ organization.website }}</h3>
+    <h3>{{ organization.imageUrl }}</h3>
+    <h3>{{ organization.facebook }}</h3>
+    <h3>{{ organization.email }}</h3>
+    <h3>{{ organization.location }}</h3>
+    <h3>{{ organization.categories.categoryName }}</h3>
+    <h3>{{ organization.tags }}</h3>
+    </div>
     </div>
   </div>
 </template>
@@ -18,6 +30,20 @@ export default {
       organizations: [
         {
           name: "JUF",
+          content:
+            "The American Jewish Council is the leading global Jewish advocacy organization, with unparalleled access to government officials, diplomats, and other world leaders. Through these relationships and international presence, AJC is able to impact opinion and policy on the issues that matter most: combating rising anti-Semitism and extremism, defending Israel’s place in the world, and safeguarding the rights and freedoms of all people.",
+          website: "www.website.com",
+          imageUrl: "",
+          facebook: "",
+          email: "",
+          location: "",
+          categories: {
+            categoryName: true
+          },
+          tags: ["social justice", "singles", "politics"]
+        },
+        {
+          name: "AJC Chicago",
           content: "thjrew lj ads lajsd jsa;d iais;d ",
           website: "www.website.com",
           imageUrl: "",
@@ -32,7 +58,7 @@ export default {
           }
         },
         {
-          name: "JUF",
+          name: "Mishkan",
           content: "thjrew lj ads lajsd jsa;d iais;d ",
           website: "www.website.com",
           imageUrl: "",
@@ -47,22 +73,7 @@ export default {
           }
         },
         {
-          name: "JUF",
-          content: "thjrew lj ads lajsd jsa;d iais;d ",
-          website: "www.website.com",
-          imageUrl: "",
-          facebook: "",
-          email: "",
-          location: "",
-          categories: {
-            categoryName: true
-          },
-          tags: {
-            tag: ""
-          }
-        },
-        {
-          name: "JUF",
+          name: "Jewish National Fund",
           content: "thjrew lj ads lajsd jsa;d iais;d ",
           website: "www.website.com",
           imageUrl: "",
